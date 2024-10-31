7361b4a9633889e16741be0a66aa9048a43e0f4d
Konstantinos Kritharidis <konstantinosk31@gmail.com>
2024-11-01 01:59:59 +0200

    Added Ex4_3 attempt

--- Lab4/Ex4_3/Ex4_3/main.c
+++ Lab4/Ex4_3/Ex4_3/main.c
@@ -1,7 +1,7 @@
 /*
  * main.c
  *
- * Created: 10/31/2024 1:20:10 AM
+ * Created: 10/31/2024 11:29:07 PM
  *  Author: User
  */ 
 
@@ -11,17 +11,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdbool.h>
 
 #define NOP() do { __asm__ __volatile__ ( "nop "); } while (0)
-#define PD0 0
-#define PD1 1
-#define PD2 2
-#define PD3 3
-#define PD4 4
-#define PD5 5
-#define PD6 6
-#define PD7 7
-#define V_REF 5
 
 void write_2_nibbles(uint8_t data){
 	uint8_t temp = PIND & 0x0f;
@@ -30,7 +22,7 @@ void write_2_nibbles(uint8_t data){
 	PORTD |= (1<<PD3);
 	NOP();
 	NOP();
-	PORTD &= ~(1<<PD3); 
+	PORTD &= ~(1<<PD3);
 	
 	PORTD = (data<<4)&(0xf0) | temp;
 	
@@ -91,7 +83,7 @@ void lcd_init(){
 }
 
 void lcd_digit(uint8_t digit){
-	lcd_data(0x30 + digit)
+	lcd_data(0x30 + digit);
 }
 
 void lcd_number(uint8_t number){
@@ -101,27 +93,78 @@ void lcd_number(uint8_t number){
 	} while(number >= 10);
 }
 
+uint8_t calc_ppm(uint8_t V_gas){
+	uint8_t val = 10000.0/129.0*((double)V_gas - 0.1);
+	if(val > 500) return 500;
+	else return val;
+}
+
+bool interrupted = true;
+
+ISR(TIMER1_COMPA_vect){
+	interrupted = true;
+}
+
+void lcd_char(char c){
+	lcd_data(c);
+}
+
+void lcd_string(char *s){
+	while(*s != '\0'){
+		lcd_char(*s);
+		s++;
+	} 
+}
+
 int main(void)
 {
 	DDRD = 0xff; //set PORTD as output
 	lcd_init();
 	_delay_ms(100);
 	
-	//   Vref = 5V, ADC1, Left adjust
-	ADMUX = (1 << REFS0) | (1 << ADLAR) | (1 << MUX0);
+	//   Vref = 5V, ADC2, Left adjust
+	ADMUX = (1 << REFS0) | (1 << ADLAR) | (1 << MUX1);
 	//   Enable, no interrupt, no conversion, 125 kHz
 	ADCSRA = (1 << ADEN) | (0 << ADSC) | (0 << ADIE) | (7 << ADPS0);
 	
-	uint8_t val;
+	// Init Timer1
+	TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10);  // CTC mode, pre-scaler 1024
+	OCR1A = 1562;                                         // Compare value for 100ms: 1562/16000000*1024 = 0.099968 = 100ms
+	TIMSK1 |= (1 << OCIE1A);                              // Enable interrupt
 	
-    while(1)
-    {
-        lcd_clear_display();
+	uint8_t val, prev_PORTB = 0;	
+	sei();
+	
+	while(1)
+	{
+		if(!interrupted){
+			_delay_ms(45);
+			PORTB = 0;
+			_delay_ms(45);
+			PORTB = prev_PORTB;
+			continue;
+		}
+		interrupted = false;
+		lcd_clear_display();
 		_delay_ms(1000);
 		ADCSRA |= 1 << ADSC;
 		while (ADCSRA & (1 << ADSC));
-		val = (ADC*V_REF)>>10;
-		lcd_number(val);
-		_delay_ms(1000);
-    }
+		uint8_t ppm = calc_ppm(ADC);
+		lcd_number(ppm);
+		if(ppm <= 70){
+			PORTB = 0;
+			lcd_string("CLEAR");
+		}
+		else{
+			lcd_string("GAS DETECTED");
+			if(ppm <= 140) PORTB = 1;
+			else if(ppm <= 210) PORTB = 2;
+			else if(ppm <= 280) PORTB = 4;
+			else if(ppm <= 350) PORTB = 8;
+			else if(ppm <= 420) PORTB = 16;
+			else PORTB = 32;
+			
+		}
+		prev_PORTB = PORTB;
+	}
 }
