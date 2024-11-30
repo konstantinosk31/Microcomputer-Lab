ad7451a1b0d5ba4b2941ce79e6637dad41c30baf
Jim Balatos <jbalatos@gmail.com>
2024-11-30 23:49:03 +0200

    Created Lab7 report.

--- Lab7/Ex7_1/Ex7_1/main.c
+++ Lab7/Ex7_1/Ex7_1/main.c
@@ -21,88 +21,88 @@ typedef uint8_t bool;
 
 /* TEMPLATE CODE FROM GIVEN ASSEMBLY */
 /* {{{ */
-	bool one_wire_reset (void)
-	{
-		SET(DDRD, 4); // set output
+bool one_wire_reset (void)
+{
+	SET(DDRD, 4); // set output
 
-		CLEAR(PORTD, 4); // sent 0
-		_delay_us(480);
+	CLEAR(PORTD, 4); // sent 0
+	_delay_us(480);
 
-		CLEAR(DDRD, 4); // set input
-		CLEAR(PORTD, 4); // disable pull-up
-		_delay_us(100);
+	CLEAR(DDRD, 4); // set input
+	CLEAR(PORTD, 4); // disable pull-up
+	_delay_us(100);
 
-		int tmp = PIND; // read PORTD
-		_delay_us(380);
-		return (tmp & (1 << 4)) == 0;
-	}
+	int tmp = PIND; // read PORTD
+	_delay_us(380);
+	return (tmp & (1 << 4)) == 0;
+}
 
-	uint8_t one_wire_receive_bit (void)
-	{
-		SET(DDRD, 4); // Set output
-		CLEAR(PORTD, 4);
-		_delay_us(2);
+uint8_t one_wire_receive_bit (void)
+{
+	SET(DDRD, 4); // Set output
+	CLEAR(PORTD, 4);
+	_delay_us(2);
 
-		CLEAR(DDRD, 4); // set input
-		CLEAR(PORTD, 4); // disable pull-up
-		_delay_us(10);
+	CLEAR(DDRD, 4); // set input
+	CLEAR(PORTD, 4); // disable pull-up
+	_delay_us(10);
 
-		uint8_t ret = PIND & 1;
-		_delay_us(49);
-		return ret;
-	}
+	uint8_t ret = PIND & 1;
+	_delay_us(49);
+	return ret;
+}
 
-	void one_wire_transmit_bit (uint8_t in)
-	{
-		SET(DDRD, 4); // Set output
-		CLEAR(PORTD, 4);
-		_delay_us(2);
+void one_wire_transmit_bit (uint8_t in)
+{
+	SET(DDRD, 4); // Set output
+	CLEAR(PORTD, 4);
+	_delay_us(2);
 
-		in &= 1; // keep only LSB
-		if (in) SET(PORTD, 4);
-		else    CLEAR(PORTD, 4);
+	in &= 1; // keep only LSB
+	if (in) SET(PORTD, 4);
+	else    CLEAR(PORTD, 4);
 
-		_delay_us(58);
-		CLEAR(DDRD, 4); // set input
-		CLEAR(PORTD, 4); // disable pull-up
-		_delay_us(1);
-	}
+	_delay_us(58);
+	CLEAR(DDRD, 4); // set input
+	CLEAR(PORTD, 4); // disable pull-up
+	_delay_us(1);
+}
 
-	uint8_t one_wire_receive_byte (void)
-	{
-		uint8_t ret = 0;
-		for (int i=0; i<8; ++i)
+uint8_t one_wire_receive_byte (void)
+{
+	uint8_t ret = 0;
+	for (int i=0; i<8; ++i)
 		ret |= (one_wire_receive_bit() << i);
-		return ret;
-	}
+	return ret;
+}
 
-	void one_wire_transmit_byte (uint8_t in)
-	{
-		for (int i=0; i<8; ++i, in >>= 1)
+void one_wire_transmit_byte (uint8_t in)
+{
+	for (int i=0; i<8; ++i, in >>= 1)
 		one_wire_transmit_bit(in);
-	}
-	/* }}} */
+}
+/* }}} */
 
-	float read_temp (void)
-	{
-		int16_t ret = 0;
+float read_temp (void)
+{
+	int16_t ret = 0;
 
-		if (one_wire_reset() != 0) return 0x8000;
-		one_wire_transmit_byte(0xCC);
-		one_wire_transmit_byte(0x44);
-		while (one_wire_receive_bit() != 1);
+	if (one_wire_reset() != 0) return 0x8000;
+	one_wire_transmit_byte(0xCC);
+	one_wire_transmit_byte(0x44);
+	while (one_wire_receive_bit() != 1);
 
-		if (one_wire_reset() != 0) return 0x8000;
-		one_wire_transmit_byte(0xCC);
-		one_wire_transmit_byte(0xBE);
+	if (one_wire_reset() != 0) return 0x8000;
+	one_wire_transmit_byte(0xCC);
+	one_wire_transmit_byte(0xBE);
 
-		ret |= one_wire_receive_byte() << 8;
-		ret |= one_wire_receive_byte();
-		return (float)(ret) / 2;
-	}
+	ret |= one_wire_receive_byte() << 8;
+	ret |= one_wire_receive_byte();
+	return (float)(ret) / 2;
+}
 
-	int main(void)
-	{
-		return 0;
-	}
+int main(void)
+{
+	return 0;
+}
 
